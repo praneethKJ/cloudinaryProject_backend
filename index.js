fecae54658721a3dc99607b702b6de82495e4822
praneeth <kodadalajp0503@gmail.com>
2023-09-26 16:24:41 +0530

    third commit

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ connectToMongo();
 
 const app = express();
 const port = process.env.PORT || 5000;
-const corsOptions = {
-  origin: 'http://localhost:5173',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
+// const corsOptions = {
+//   origin: 'http://localhost:5173',
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// };
 app.use(cors({
-  'allowedHeaders':['content-Type'],
-  'origin':'*',
-  'preflightContinue':true
+  'allowedHeaders': ['content-Type'],
+  'origin': '*',
+  'preflightContinue': true
 }));
 app.use(express.json());
 // Routes for uploading and playing videos
