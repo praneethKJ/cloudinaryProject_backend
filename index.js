7258783deba04493c656bfa5775e864cde7cbd3c
praneeth <kodadalajp0503@gmail.com>
2023-09-26 21:59:08 +0530

    fourth commit

--- index.js
+++ index.js
@@ -10,16 +10,10 @@ app.use(cors());
 connectToMongo();
 
 const port = process.env.PORT || 5000;
-// const corsOptions = {
-//   origin: 'https://bespoke-gecko-c7e64a.netlify.app/',
-//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-// };
 app.use(express.json());
 
 // Routes for uploading and playing videos
 app.use('/api/upload', uploadRoutes);
-// app.use('/api/play', require('./routes/play'));
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
