98d9cc46daa27946673b61a79bec51f1ccd2ad5e
praneeth <kodadalajp0503@gmail.com>
2023-09-26 17:06:27 +0530

    third commit

--- middleware.js
+++ middleware.js
@@ -1,6 +1,6 @@
 const enableCORS = function (app) {
     app.all("/", function (req, res, next) {
-      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Origin', 'https://bespoke-gecko-c7e64a.netlify.app/');
       res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
       res.header('Access-Control-Allow-Headers', 'Content-Type, token, Content-Length, X-Requested-With, *');
       next();
