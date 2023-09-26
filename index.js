7239dc2b14dc012698be0c5b71288459fc496869
praneeth <kodadalajp0503@gmail.com>
2023-09-26 17:34:32 +0530

    first commit

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-
-
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
 import { connectToMongo } from './db.js';
 import uploadRoutes from './routes/upload.js';
-// Call the function to connect to MongoDB
-connectToMongo();
 
 const app = express();
+app.use(cors());
+connectToMongo();
+
 const port = process.env.PORT || 5000;
-const corsOptions = {
-  origin: 'https://bespoke-gecko-c7e64a.netlify.app/',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: 'https://bespoke-gecko-c7e64a.netlify.app/',
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// };
 app.use(express.json());
 
 // Routes for uploading and playing videos
